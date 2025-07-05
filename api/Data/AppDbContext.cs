f88c57d146ddfa73c753d9464e474cc1e831cb7e
Obaydullah <obaydullahkhaan@gmail.com>
2025-07-05 23:13:13 +0600

    database configured with ef core

---
+++ api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
